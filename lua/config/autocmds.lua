6d1d224e0296fe64c1ab8355e47d8118229db5ac
Jimmie Fulton <jimmie.fulton@naxgrp.com>
2023-11-10 19:23:38 -0800

    Disable DirChanged autocmd

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,9 +1,9 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
-vim.api.nvim_create_autocmd({ "DirChanged" }, {
-  pattern = "global",
-  callback = function(ev)
-    vim.cmd("! wezterm cli set-tab-title " .. vim.fs.basename(vim.loop.cwd()))
-  end,
-})
+-- vim.api.nvim_create_autocmd({ "DirChanged" }, {
+--   pattern = "global",
+--   callback = function(ev)
+--     vim.cmd("! wezterm cli set-tab-title " .. vim.fs.basename(vim.loop.cwd()))
+--   end,
+-- })
