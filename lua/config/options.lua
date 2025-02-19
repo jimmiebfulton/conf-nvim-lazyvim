168de415c13ee637bf40fb660e1e159299423382
Jimmie Fulton <jimmie.fulton@gmail.com>
2025-02-18 23:49:36 -0500

    neovide: re-enable left meta key

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -14,8 +14,6 @@ if vim.g.neovide then
   vim.g.neovide_cursor_vfx_particle_lifetime = 2.0
   vim.g.neovide_cursor_vfx_opacity = 400.0
   vim.g.neovide_fullscreen = false
-  vim.g.neovide_transparency = 0.95
-  vim.g.neovide_normal_opacity = 0.95
   vim.g.neovide_input_macos_option_key_is_meta = "only_left"
   vim.g.neovide_transparency = 0.98
   vim.g.neovide_normal_opacity = 0.98
