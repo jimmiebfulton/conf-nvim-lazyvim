41b721ea33a8e17d92a3f750a1e8dccd7f082468
Jimmie Fulton <jimmie.fulton@gmail.com>
2025-02-18 23:49:36 -0500

    neovide: decrease transparence

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -17,6 +17,8 @@ if vim.g.neovide then
   vim.g.neovide_transparency = 0.95
   vim.g.neovide_normal_opacity = 0.95
   vim.g.neovide_input_macos_option_key_is_meta = "only_left"
+  vim.g.neovide_transparency = 0.98
+  vim.g.neovide_normal_opacity = 0.98
 end
 
 -- vim.g.firenvim_config = {
