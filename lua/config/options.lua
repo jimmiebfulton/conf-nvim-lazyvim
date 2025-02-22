d0e5d3ccb3ebea141f00b099ddac90de414c76e1
Jimmie Fulton <jimmie.fulton@gmail.com>
2025-02-22 11:35:48 -0800

    neovide: disable alt dead keys

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -16,4 +16,5 @@ if vim.g.neovide then
   vim.g.neovide_fullscreen = false
   vim.g.neovide_transparency = 0.95
   vim.g.neovide_normal_opacity = 0.95
+  vim.g.neovide_input_macos_option_key_is_meta = "only_left"
 end
