9b4fccad42e0120878915592f1d816165bf8f89c
Jimmie Fulton <jimmie.fulton@gmail.com>
2025-03-12 23:07:01 -0700

    blink: extensive customization

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,30 +1,69 @@
 return {
   "saghen/blink.cmp",
+  dependencies = {
+    { "xzbdmw/colorful-menu.nvim" },
+  },
   lazy = false,
   opts = {
     keymap = {
       preset = "super-tab",
-      ["<CR>"] = { "accept", "fallback" },
+      ["<CR>"] = { "select_and_accept" },
       ["<Up>"] = { "select_prev", "fallback" },
       ["<C-k>"] = { "select_prev", "fallback" },
       ["<Down>"] = { "select_next", "fallback" },
       ["<C-j>"] = { "select_next", "fallback" },
+      ["<C-M-j>"] = { "scroll_documentation_down" },
+      ["<C-M-k>"] = { "scroll_documentation_up" },
     },
     completion = {
+      accept = { auto_brackets = { enabled = false } },
+      menu = {
+        auto_show = true,
+        draw = {
+          columns = {
+            { "kind_icon", "label", gap = 1 },
+            { "kind" },
+          },
+          components = {
+            label = {
+              text = function(ctx)
+                return require("colorful-menu").blink_components_text(ctx)
+              end,
+              highlight = function(ctx)
+                return require("colorful-menu").blink_components_highlight(ctx)
+              end,
+            },
+          },
+          treesitter = { "lsp" },
+        },
+      },
+
+      -- Show documentation when selecting a completion item
+      documentation = { auto_show = true, auto_show_delay_ms = 500 },
+
+      keyword = { range = "full" },
+
       list = {
         selection = {
           preselect = true,
-          auto_insert = true,
+          auto_insert = false,
         },
       },
+      -- Display a preview of the selected item on the current line
+      ghost_text = { enabled = true },
+
       trigger = {
-        show_in_snippet = false,
+        show_in_snippet = true,
       },
     },
+    signature = { enabled = false },
+
     cmdline = {
       enabled = true,
       keymap = {
-        preset = "cmdline",
+        -- preset = "cmdline",
+        preset = "super-tab",
+        ["<CR>"] = { "accept_and_enter", "fallback" },
         ["<Up>"] = { "select_prev" },
         ["<C-k>"] = { "select_prev" },
         ["<Down>"] = { "select_next" },
@@ -50,13 +89,13 @@ return {
         list = {
           selection = {
             -- When `true`, will automatically select the first item in the completion list
-            preselect = false,
+            preselect = true,
             -- When `true`, inserts the completion item automatically when selecting it
             auto_insert = true,
           },
         },
         -- Whether to automatically show the window when new completion items are available
-        menu = { auto_show = false },
+        menu = { auto_show = true },
         -- Displays a preview of the selected item on the current line
         ghost_text = { enabled = true },
       },
