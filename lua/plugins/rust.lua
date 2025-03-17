034323af84bb52f8ea8929c66a1b98450ba018ba
Jimmie Fulton <jimmie.fulton@gmail.com>
2025-03-17 10:27:07 -0700

    avante: add plugin, change Rust binds

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -1,9 +1,17 @@
 return {
 
   {
-    "saecki/crates.nvim",
+    "Saecki/crates.nvim",
     tag = "stable",
+    event = { "BufRead Cargo.toml" },
     opts = {
+      completion = {
+        crates = {
+          enabled = true, -- disabled by default
+          max_results = 10, -- The maximum number of search results to display
+          min_chars = 3,
+        },
+      },
       popup = {
         autofocus = true,
         keys = {
@@ -21,16 +29,16 @@ return {
       lsp = {
         enabled = true,
         on_attach = function(_, buffer)
-          vim.keymap.set("n", "<leader>ac", function()
+          vim.keymap.set("n", "<leader>cC", function()
             require("crates").show_crate_popup()
           end, { desc = "Show Crate", buffer = buffer })
-          vim.keymap.set("n", "<leader>af", function()
+          vim.keymap.set("n", "<leader>cF", function()
             require("crates").show_features_popup()
           end, { desc = "Show Features", buffer = buffer })
-          vim.keymap.set("n", "<leader>ad", function()
+          vim.keymap.set("n", "<leader>cD", function()
             require("crates").show_dependencies_popup()
           end, { desc = "Show Dependencies", buffer = buffer })
-          vim.keymap.set("n", "<leader>av", function()
+          vim.keymap.set("n", "<leader>cV", function()
             require("crates").show_versions_popup()
           end, { desc = "Show Versions", buffer = buffer })
         end,
